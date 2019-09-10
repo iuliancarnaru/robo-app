dec38c3306713e84ba4e58e4199f202d8fb22f40
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-09-10 14:35:57 +0100

    adding  thunk and middleware

--- src/App.js
+++ src/App.js
@@ -4,46 +4,38 @@ import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import Scroll from "./Scroll";
 import ErrorBoundry from "./ErrorBoundry";
-import { setSearchField } from "./redux/actions";
+import { setSearchField, requestRobots } from "./redux/actions";
 // import { robots } from "./robots";
 
 const mapStateToProps = state => {
   return {
-    searchFiled: state.searchFiled
+    searchFiled: state.searchRobots.searchFiled,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value))
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   };
 };
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      robots: []
-    };
-  }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }))
-      .catch(error => console.log(error));
+    this.props.onRequestRobots();
   }
 
   render() {
-    const { robots } = this.state;
-    const { searchFiled, onSearchChange } = this.props;
-
+    const { searchFiled, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchFiled.toLowerCase());
     });
 
-    return !robots.length ? (
+    return isPending ? (
       <h1>Loading ...</h1>
     ) : (
       <div style={{ padding: "20px" }}>
